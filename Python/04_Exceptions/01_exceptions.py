f48bae8e7c0ad430a7eecc21d0c0827377eef836
Onkar Ghadage <147942562+OnkarGhadage@users.noreply.github.com>
2025-10-08 19:04:58 +0530

    Create 01_exceptions.py
    Add solution for Exception.

---
+++ Python/04_Exceptions/01_exceptions.py
@@ -0,0 +1,12 @@
+# This solution is written and tested by me
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+
+for i in range(T):
+    a, b = map(str, input().split(" "))
+    try:
+        print(int(a)//int(b))
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+    except ValueError as e:
+        print("Error Code:",e)
