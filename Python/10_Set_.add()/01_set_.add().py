5074b7471f35b8c48dee421108fabc7b7b5b21d7
Onkar Ghadage <147942562+OnkarGhadage@users.noreply.github.com>
2025-10-14 19:06:07 +0530

    Create 01_set_.add().py
    Add solution for Set .add().

---
+++ Python/10_Set_.add()/01_set_.add().py
@@ -0,0 +1,7 @@
+# This solution is written and tested by me
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input())
+stamps = set()
+for i in range(N):
+    stamps.add(input())
+print(len(stamps))
