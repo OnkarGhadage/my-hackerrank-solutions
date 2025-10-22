31cab6e1d332f66eb79ae4e82b8f48d5357c057e
Onkar Ghadage <147942562+OnkarGhadage@users.noreply.github.com>
2025-10-22 09:49:41 +0530

    Add Set .difference() Operation implementation
    Implement set difference operation to count unique elements in first set.

---
+++ Python/20_Set_.difference()_Operation/01_set_.difference()_operation.py
@@ -0,0 +1,7 @@
+# This solution is written and tested by me
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = input()
+e = set(input().split())
+m = input()
+f = set(input().split())
+print(len(e-f))
