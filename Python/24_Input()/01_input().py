154f53d35fd252f650dd6ebe17a838d14cad1828
Onkar Ghadage <147942562+OnkarGhadage@users.noreply.github.com>
2025-10-24 10:02:07 +0530

    Add solution for Input().
    Implement input evaluation and comparison logic

---
+++ Python/24_Input()/01_input().py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x, k = map(int, input().split())
+result = eval(input())
+if result == k:
+    print(True)
+else:
+    print(False)
