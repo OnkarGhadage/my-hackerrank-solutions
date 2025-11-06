f58cea6f6ec4dae34fcfa56315fa150f69ba370d
Onkar Ghadage <147942562+OnkarGhadage@users.noreply.github.com>
2025-11-06 07:15:39 +0530

    Create 01_validating_email_addresses_with_a_filter.py

---
+++ Python/45_Validating Email Addresses With a Filter/01_validating_email_addresses_with_a_filter.py
@@ -0,0 +1,18 @@
+import re
+def fun(s):
+    # return True if s is a valid email, else return False
+    is_valid = re.match(r'^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$',s)
+    return is_valid
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
