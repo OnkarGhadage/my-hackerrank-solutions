b99a05c7c104336c9ebb647e4a96b851e1c1c48a
OnkarGhadage <onkaryg@gmail.com>
2025-11-15 20:05:55 +0530

    Add solution of Map and Lambda Function

---
+++ Python/53_Map_and_Lambda_Function/01_map_and_lambda_function.py
@@ -0,0 +1,14 @@
+cube = lambda x: x**3 # complete the lambda function 
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    series = []
+    a, b = 0, 1
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a+b
+    return series
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
